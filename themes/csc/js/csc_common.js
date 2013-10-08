136f5df0d129302f2fbd31d6276f008ae3e160ba
Jed Verity <jed@dataverse.me>
2013-10-08 12:17:03 -0400

    about us styling

--- themes/csc/js/csc_common.js
+++ themes/csc/js/csc_common.js
@@ -1,5 +1,9 @@
 (function($){
     $(window).load(function(){
         $(".add-scroll").mCustomScrollbar();
+
+        $('.csc-directors-wrapper').on('click', function(){
+          $(this).find('a').click();
+        });
     });          
 })(jQuery);

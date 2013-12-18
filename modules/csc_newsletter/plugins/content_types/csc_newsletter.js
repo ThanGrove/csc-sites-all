32692a63573126c2ab3ba13515b03f5efa10b51c
Jed Verity <jed@dataverse.me>
2013-12-18 14:47:18 -0500

    latest

--- modules/csc_newsletter/plugins/content_types/csc_newsletter.js
+++ modules/csc_newsletter/plugins/content_types/csc_newsletter.js
@@ -2,28 +2,36 @@
 
   $(document).ready(function () {
     $('.mce-button').on("click",function(e){
-  	 var f = $(this).closest("form");
+     var f = $(this).closest("form");
      e.preventDefault();
+     
+     //Check email field
+     if($('#mce-EMAIL').val() == ''){
+      alert('Email field is required');
+      $('#mce-EMAIL').focus();
+      return;
+     }
+     
      $( "#dialog-modal" ).dialog({
-  		 height: 250,
+  		 height: 270,
        width: 370,
   		 modal: true,
        buttons: {
-        "Close": function() { $(this).dialog("close");},
-        "Accept": function(){
-                    if( $('#news-1').prop('checked') ) 
-                      $('#mce-group-16045-0').prop('checked', true );
-                    else
-                      $('#mce-group-16045-0').prop('checked', false);
-
-                    if( $('#modal-form #news-2').prop('checked') ) 
-                      $('#mce-group-16045-1').prop('checked', true);
-                    else
-                      $('#mce-group-16045-1').prop('checked', false);
-                      
-                    f.submit();
-                    $(this).dialog("close");
-                  }
+        "Cancel":    function() { $(this).dialog("close");},
+        "Subscribe": function(){
+                      if( $('#news-1').prop('checked') ) 
+                        $('#mce-group-16045-0').prop('checked', true );
+                      else
+                        $('#mce-group-16045-0').prop('checked', false);
+  
+                      if( $('#modal-form #news-2').prop('checked') ) 
+                        $('#mce-group-16045-1').prop('checked', true);
+                      else
+                        $('#mce-group-16045-1').prop('checked', false);
+                        
+                      f.submit();
+                      $(this).dialog("close");
+                    }
        }
   	 });
     });

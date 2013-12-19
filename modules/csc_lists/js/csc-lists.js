90b046fb3d6c6cc159aadfc4568a7b6c13e27dd9
Jed Verity <jed@dataverse.me>
2013-12-19 08:44:09 -0500

    form fix for list submission

--- modules/csc_lists/js/csc-lists.js
+++ modules/csc_lists/js/csc-lists.js
@@ -1,7 +1,7 @@
 (function($) {
   $(function(){
     $('#audience-filter').on('click', 'a', function(event){
-      event.stopPropagation();
+      event.preventDefault();
       document.getElementById('audience-input').value = event.target.getAttribute('data-value');
       document.getElementById('csc-list-form').submit();
       return false;

2ca91aaec71574442cd4187fde7dcc9ddd26f134
Jed Verity <jed@dataverse.me>
2014-03-05 13:52:49 -0500

    merge dev

--- themes/csc/js/csc_common.js
+++ themes/csc/js/csc_common.js
@@ -1,6 +1,12 @@
 (function($){
     $(window).load(function(){
         $(".add-scroll").mCustomScrollbar();
+        
+      	$("#calendar-link").fancybox({
+      		ajax : {
+      		    type	: "GET"
+      		}
+      	});
 
         //$('.csc-directors-wrapper').on('click', function(){
           //$(this).find('a').click();

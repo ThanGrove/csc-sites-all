e6dda89f87cccafd47333e47503e6404a612bbdf
Jed Verity <jed@dataverse.me>
2014-05-01 12:39:51 -0400

    adding target blank to menu

--- themes/csc/js/csc_common.js
+++ themes/csc/js/csc_common.js
@@ -1,6 +1,7 @@
 (function($){
     $(window).load(function(){
         $(".add-scroll").mCustomScrollbar();
+        $('#m-3223').find('a')[0].target = '_blank';
         
         //$("#calendar-link").fancybox({
           //ajax : {

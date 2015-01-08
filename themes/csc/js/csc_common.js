efba3a63c75f0adbf0eaecff215fd2298bfa54e8
ThanGrove <than@virginia.edu>
2015-01-08 12:56:13 -0500

    adding JS to hide map link if empty or no address given. If empty, substitute TBA

--- themes/csc/js/csc_common.js
+++ themes/csc/js/csc_common.js
@@ -3,24 +3,28 @@
         $(".add-scroll").mCustomScrollbar();
         if($('#m-3223').find('a').length > 0) { $('#m-3223').find('a')[0].target = '_blank'; }
         
-        //$("#calendar-link").fancybox({
-          //ajax : {
-              //type	: "GET"
-          //}
-        //});
-
-        //$('.csc-directors-wrapper').on('click', function(){
-          //$(this).find('a').click();
-        //});
-        
         // Hide Google Map link if Place is TBA
-        var pf = $('.pane-node-field-location');
-        if(pf.length > 0) {
-	        var pval = pf.find('.adr .fn').text();
-	        if(pval && pval == 'TBA') {
-	        	$('.map-link .location.map-link').hide();
+        var pf = $('.pane-node-field-location, .views-field-field-location');
+        pf.each(function() {
+	        var pval = $(this).find('.adr .fn').text();
+	        if(pval && pval == 'TBA' || pval == '') {
+	        	$(this).find('.map-link .location.map-link').hide();
 	        }
-	      }
+	      });
+	      // Hide map link if no coordinates/address given
+	      $('.location.map-link').each(function() {
+	      	var a = $(this).find('a');
+	      	if(a.attr('href') == 'http://maps.google.com?q=%2C+%2C+%2C+%2C+us') {
+	      		$(this).hide();
+	      	}
+	      });
+	      // if field location empty, add TBA
+	      $('.views-field-field-location').each(function() {
+	      	var cnt = $(this).find('.field-content').html();
+	      	if(cnt == '') {
+	      		$(this).find('.field-content').text('TBA');
+	      	}
+	      });
 
     });          
 })(jQuery);

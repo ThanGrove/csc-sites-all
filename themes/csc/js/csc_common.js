ba6c77d880336d448a89ecf74216a2f566c2f01a
ThanGrove <than@virginia.edu>
2015-01-08 09:07:34 -0500

    update to permissions/ownership (local) and adding csc-date for TBA and creating csc project in info files.

--- themes/csc/js/csc_common.js
+++ themes/csc/js/csc_common.js
@@ -1,7 +1,7 @@
 (function($){
     $(window).load(function(){
         $(".add-scroll").mCustomScrollbar();
-        $('#m-3223').find('a')[0].target = '_blank';
+        if($('#m-3223').find('a').length > 0) { $('#m-3223').find('a')[0].target = '_blank'; }
         
         //$("#calendar-link").fancybox({
           //ajax : {
@@ -12,6 +12,15 @@
         //$('.csc-directors-wrapper').on('click', function(){
           //$(this).find('a').click();
         //});
+        
+        // Hide Google Map link if Place is TBA
+        var pf = $('.pane-node-field-location');
+        if(pf.length > 0) {
+	        var pval = pf.find('.adr .fn').text();
+	        if(pval && pval == 'TBA') {
+	        	$('.map-link .location.map-link').hide();
+	        }
+	      }
 
     });          
 })(jQuery);

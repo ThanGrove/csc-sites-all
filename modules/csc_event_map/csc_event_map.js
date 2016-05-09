0bce67788b1c87574a4c5f7eb7bb051973b097d0
ThanGrove <than@virginia.edu>
2016-05-09 10:30:27 -0400

    fixing map js error and using brandon-light for body font with color 333, best solution yet

--- modules/csc_event_map/csc_event_map.js
+++ modules/csc_event_map/csc_event_map.js
@@ -1,14 +1,17 @@
 (function($) {
   $(function(){
-    var map = document.getElementById('map-frame'),
-        src = map.src.replace('iwto=A',''),
-        wto;
-
-    $(window).resize(function(){
-      clearTimeout(wto);
-      wto = setTimeout(function() {
-        map.src = src;
-      }, 300);
-    });
+    var map = document.getElementById('map-frame');
+    
+    if (typeof(map) != "undefined" && typeof(map.src) != "undefined") {
+       var  src = map.src.replace('iwto=A',''),
+            wto;
+    
+        $(window).resize(function(){
+          clearTimeout(wto);
+          wto = setTimeout(function() {
+            map.src = src;
+          }, 300);
+        });
+      }
   });
 })(jQuery);
